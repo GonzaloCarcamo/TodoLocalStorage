32c64d2ebcd484f8b116f2a33749741a57a5918f
goncmodev <gon.cmo.dev@gmail.com>
2021-04-29 20:25:18 +0100

    validate form

--- js/app.js
+++ js/app.js
@@ -16,5 +16,28 @@ function eventListeners() {
 function addIdea(e) {
         e.preventDefault();
 
-        console.log('test');
+        // Where user writes...
+        const idea = document.querySelector('#idea').value;
+        
+        // Validation
+        if(idea === ''){
+                showError('A message cannot be empty');
+                return; // block code
+        }
+
+        console.log('agregando')
+}
+
+// Show error message
+function showError(error) {
+        const messageError = document.createElement('p');
+        messageError.textContent = error;
+        messageError.classList.add('text-center');
+        const errorDiv = document.querySelector('#errorDiv');
+        errorDiv.appendChild(messageError);
+
+        setTimeout(() => {
+                messageError.remove();
+        }, 2000)
+
 }
